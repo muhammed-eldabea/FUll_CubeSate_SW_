4a98bbe6f7c9be99f6e93e4487e79e3091bd7ad9
muhammed eldabea <43865045+muhammed-eldabea@users.noreply.github.com>
2021-05-22 03:14:41 +0200

    Add files via upload

--- OBC/OBC.ino
+++ OBC/OBC.ino
@@ -1,110 +1,135 @@
 
-
-/*==================================================================*/
-
-                /* Include the main Library needded */
-
-#include <Wire.h>  
-
-
-/*===================================================================*/
-                    /* System Address Defination   */ 
-#define OBC_ADD  1
-#define EPS_ADD  2
-#define COM_ADD  3
-#define PAYLOAD_ADD  4 
-
-/*===================================================================*/
-                    /* COMMAND IDs  */
-
-#define PayLoad_Start_Action  1 
-#define Send_Telemetry_Data   2 
-#define Delay_for_Time        3 
+int counter = 0 ; 
+int Data[6] = {0,} ; 
+int Telemtry_From_EPS [3] = {0,} ;
 
 
 
 
-/*===================================================================*/
-                    /* MODES IDs  */ 
-#define Initial_Mode    1 
-#define Imaging_Mode    2 
-#define DownLoad_Mode   3 
-#define Comm_Mode
-
-
-
-
-/*===================================================================*/
-                    /* Tasks Decleration  */
-
-
-
-
-/*===================================================================*/
-            /* Defination for Data structre used */
-
-unsigned char Command_Buffer[20] = {0,} ;
-unsigned char Telemetry_Buffer[20] = {0,} ;  
-
-
-/*===================================================================*/
+#include <Wire.h>
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+  pinMode(8,INPUT) ; 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-} 
+delay(5000);
 
+OBC_INITIAL_MODE() ;  /*initial Mode */
+delay(4000);
+OBC_READ_FROM_EPS()  ; /*HouseKeeping */ 
+delay(2000);
+OBC_READ_FROM_COM()  ; /*Communication mode*/ 
+OBC_SEND_COMMAND_TO_EPS ()  ; /*Payload Run*/
+OBC_DOWNLOAD_MODE() ; 
 
-
-/*===================================================================*/
-                  
-                  /* Main Tasks Defiantion */
-void OBC_TeleComand_Task() 
-{
   
-}  
+}
 
 
+ void OBC_READ_FROM_COM() 
+ {
+   int counter= 0 ; 
+  Wire.requestFrom(8, 3);    // request 6 bytes from slave device #8
 
-/*===================================================================*/
-
-                        /* Sub-Task */ 
-
-void OBC_GetDataFrom_COM() 
-{
-    Get_telemetry_Data_FROM_COM() ; 
-} 
-
-
-
-
-
-
-/*===================================================================*/
-              
-                 /*Hardware Communication Task */ 
-
-void  Get_telemetry_Data_FROM_System(unsigned char system_ID , unsigned char telemetry_size) 
-{ 
- unsigned char counter = 0 ;  
-   Wire.requestFrom(system_ID, telemetry_size);    // request 6 bytes from slave device #8
-  while (Wire.available()) { // slave may send less than requested
-    Telemetry_Buffer[counter] = Wire.read();
-    counter++ ;  
+  while ((Wire.available()) && (counter  != 6 )) { // slave may send less than requested
+    Telemtry_From_EPS[counter] = Wire.read();      // receive a byte as character
+    counter++;                                     // print the character
   }
-  
+  delay(500);
+ } 
+ 
+ 
+ void OBC_DOWNLOAD_MODE() 
+ { 
+ Wire.beginTransmission(8); // transmit to device #8
+	wire,write(0) ; 
+	wire,write(1) ; /*set the comm flag */
+	wire.write(Telemtry_From_EPS[0]) ; /*Temp value*/
+	wire.write(Telemtry_From_EPS[1]) ; /* Voltage value */
+	wire.write(Telemtry_From_EPS[2]) ; /*Max Value */	 
+	if (Telemtry_From_EPS[0] > 25) 
+	{
+		wire.write(1) ; 
+	}
+	else 
+	{
+		wire.write(0) ; 
+	}
+  Wire.endTransmission();    // stop transmitting
+
+	 
+ }
+ 
+    
+
+void OBC_READ_FROM_EPS() 
+{
+   int counter = 0 ;
+  Wire.requestFrom(9, 3);    // request 6 bytes from slave device #8
+
+  while ((Wire.available()) && (counter  != 6 )) { // slave may send less than requested
+    Data[counter] = Wire.read(); // receive a byte as character
+         counter++;    // print the character
+  }
+}
+
+void RUN_TEST() 
+{
+for (int i  = 0 ;  i < 6 ; i++) 
+{
+  Serial.println(Data[i])  ;  
+  }    
+}
+
+void OBC_INITIAL_MODE() 
+{
+  OBC_SEND_PING_TO_COMM ()  ; 
+}
+
+
+
+void OBC_SEND_COMMAND_TO_EPS(int Command) 
+{
+    Wire.beginTransmission(9); // transmit to device #8
+    Wire.write(Command);              // sends one byte
+  Wire.endTransmission();    // stop transmitting
+
+}
+
+
+void OBC_SEND_PING_TO_COMM () 
+{
+  Wire.beginTransmission(8); // transmit to device #8
+  Wire.write(1);
+for  (int i = 0 ; i<4 ; i++) 
+{
+	Wire.write(0);
+}
+  Wire.endTransmission();    // stop transmitting
+
+	
 } 
 
-void OBC_Send_Command (unsigned char system_ID ,unsigned char command  ) 
-{ 
-    Wire.beginTransmission(system_ID); // transmit to device ID
-    Wire.write(command);              // sends one command
-    Wire.endTransmission();    // stop transmitting
 
-  
+
+void OBC_SEND_COMMAND_TO_EPS () 
+{
+  Wire.beginTransmission(9); // transmit to device #8
+  Wire.write(Data[1]);
+  Wire.write(Data[2]);
+  if (Data[5] == 2)
+  { 
+/*to test charge mode */
+    Wire.write(1);
+  }
+    else {
+      Wire.write(0);
+    }
+
+  // sends one byte
+  Wire.endTransmission();    // stop transmitting
 }
