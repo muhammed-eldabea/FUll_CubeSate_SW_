a47de051753a293f3afea8ebb018f7b113fedffc
muhammed eldabea <43865045+muhammed-eldabea@users.noreply.github.com>
2021-05-22 03:13:08 +0200

    Add files via upload

--- COM/COM.ino
+++ COM/COM.ino
@@ -1,16 +1,18 @@
-// Wire Slave Sender
-// by Nicholas Zambetti <http://www.zambetti.com>
 
-// Demonstrates use of the Wire library
-// Sends data as an I2C/TWI slave device
-// Refer to the "Wire Master Reader" example for use with this
 
-// Created 29 March 2006
+#include <Wire.h>
 
-// This example code is in the public domain.
+
+
+
+#define SEND_PING_TO_GROUND 12
+#define SEND_TELM_TO_GROUND 11 
+#define GET_COMMAND_FROM_GROUND 10 
 
 int Data[6] = {6,7,8,9,10,11} ; 
-#include <Wire.h>
+
+/*  << PING >> << COM_FLAG >> <<T1>> <<T2>> <<T3>>                           */
+int Data_FROM_OBC[6] = {0,} ;  
 
 void setup() {
   Wire.begin(8);                // join i2c bus with address #8
@@ -21,24 +23,60 @@ void setup() {
 
 
 void loop() {
-  delay(100);
+  delay(100); 
+  if (Data_FROM_OBC[0] == 1 ) 
+  {
+	  Serial.write(SEND_PING_TO_GROUND) ; 
+	  Data_FROM_OBC[0] = 0 ;  /*clear the PING_FLAG */
+  }
+  if (Data_FROM_OBC[1] == 1 ) 
+  {
+	  Serial.write(SEND_TELM_TO_GROUND) ; 
+	  COMM_SEND_TELM_DATA() ; 
+	  Serial.write(GET_COMMAND_FROM_GROUND) ; 
+	  COMM_GET_COMMAND_FRON_GS() ; 
+	  Data_FROM_OBC[1] = 0 ;  /*clear the COMM_FLAG */
+  }
+	
+  
+  
 }
 
-// function that executes whenever data is requested by master
-// this function is registered as an event, see setup()
+
 void requestEvent() {
    for (int i =  0 ; i < 6 ; i++) 
   { 
-  Wire.write(Data[i]); // respond with message of 6 bytes
-  // as expected by master
+  Wire.write(Data[i]); 
+  
 }
 }
 
 void receiveEvent(int howMany) {
-  while (1 < Wire.available()) { // loop through all but the last
-    char c = Wire.read(); // receive byte as a character
-    Serial.print(c);         // print the character
+  
+  volatile int counter = 0 ; 
+  while ((1 < Wire.available()) && (counter != 6)) { // loop through all but the last
+    Data_FROM_OBC[counter] = Wire.read(); 
+    counter++ ;         
   }
-  int x = Wire.read();    // receive byte as an integer
-  Serial.println(x);         // print the integer
+         
+}
+
+void COMM_SEND_TELM_DATA() 
+{
+	for (int i = 2 ; i<6 ; i++)
+	{
+		Serial.write(Data_FROM_OBC[i]) ;  
+	}
+} 
+
+
+void COMM_GET_COMMAND_FRON_GS() 
+{ 
+volatile int counter = 0 ; 
+	while ( (Serial.available() > 1) && (counter != 6 ) )
+	{
+		Data[counter]=Serial.read() ; 
+		counter++ ; 
+	}
+	
 }
