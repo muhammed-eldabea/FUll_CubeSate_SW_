ca512bca0475f94bfb95867a40cc46f68c883e26
muhammed eldabea <43865045+muhammed-eldabea@users.noreply.github.com>
2021-07-09 18:44:54 +0200

    Update COM.ino

--- COM/COM.ino
+++ COM/COM.ino
@@ -4,43 +4,68 @@
 
 
 
-
+#define SYNCH_DONE         99 
 #define SEND_PING_TO_GROUND 12
 #define SEND_TELM_TO_GROUND 11 
 #define GET_COMMAND_FROM_GROUND 10 
 
-int Data[6] = {6,7,8,9,10,11} ; 
+int Data[5] = {0,} ; 
+
+String Data_str ; 
 
 /*  << PING >> << COM_FLAG >> <<T1>> <<T2>> <<T3>>                           */
-int Data_FROM_OBC[6] = {0,} ;  
+int Data_FROM_OBC[6] = {0,0,0,0,0,0} ;  
 
 void setup() {
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onRequest(requestEvent); // register event
   Wire.onReceive(receiveEvent);
-  Serial.begin(9600) ; 
+  Serial.begin(38400) ; 
 }
 
 
 void loop() {
-  delay(100); 
+ // delay(4010); 
+  for (int i = 0 ; i<6 ; i++) 
+  {
+    Serial.println(Data_FROM_OBC[i]) ; 
+  }
   if (Data_FROM_OBC[0] == 1 ) 
   {
-	  Serial.write(SEND_PING_TO_GROUND) ; 
-	  Data_FROM_OBC[0] = 0 ;  /*clear the PING_FLAG */
+    Serial.write(SEND_PING_TO_GROUND) ;  
+  /* while(Serial.read() != SYNCH_DONE) {
+    Serial.write(SEND_PING_TO_GROUND) ;}*/
+    Data_FROM_OBC[0] = 0 ;  /*clear the PING_FLAG */
   }
+delay(1000) ; 
+  
   if (Data_FROM_OBC[1] == 1 ) 
   {
-	  Serial.write(SEND_TELM_TO_GROUND) ; 
-	  COMM_SEND_TELM_DATA() ; 
-	  Serial.write(GET_COMMAND_FROM_GROUND) ; 
-	  COMM_GET_COMMAND_FRON_GS() ; 
-	  Data_FROM_OBC[1] = 0 ;  /*clear the COMM_FLAG */
+   Serial.write(GET_COMMAND_FROM_GROUND) ;
+  /* 
+   while(Serial.read() != SYNCH_DONE) 
+   {
+       Serial.write(GET_COMMAND_FROM_GROUND) ;
+   }*/ 
+    COMM_GET_COMMAND_FRON_GS() ; 
+
+    delay(15) ; 
+    Serial.write(SEND_TELM_TO_GROUND) ; 
+  /*  while(Serial.read() != SYNCH_DONE) {
+    Serial.write(SEND_TELM_TO_GROUND) ;}
+    */
+    COMM_SEND_TELM_DATA() ; 
+    
+    Data_FROM_OBC[1] = 0 ;  /*clear the COMM_FLAG */
   }
-	
   
-  
+   
+  for (int i = 0 ; i <5 ; i++) 
+  {
+    Serial.println(Data[i]) ; 
+  }
 }
+
 
 
 void requestEvent() {
@@ -52,31 +77,31 @@ void requestEvent() {
 }
 
 void receiveEvent(int howMany) {
-  
-  volatile int counter = 0 ; 
-  while ((1 < Wire.available()) && (counter != 6)) { // loop through all but the last
+   
+  for (int counter = 0 ; counter<5 ; counter++) { // loop through all but the last
     Data_FROM_OBC[counter] = Wire.read(); 
-    counter++ ;         
+            
   }
          
 }
 
 void COMM_SEND_TELM_DATA() 
 {
-	for (int i = 2 ; i<6 ; i++)
-	{
-		Serial.write(Data_FROM_OBC[i]) ;  
-	}
+  for (int i = 2 ; i<6 ; i++)
+  {
+    Serial.write(Data_FROM_OBC[i]) ;  
+  }
 } 
 
 
 void COMM_GET_COMMAND_FRON_GS() 
 { 
-volatile int counter = 0 ; 
-	while ( (Serial.available() > 1) && (counter != 6 ) )
-	{
-		Data[counter]=Serial.read() ; 
-		counter++ ; 
-	}
-	
+ 
+  for (int counter = 0 ; counter<5 ; counter++) 
+    {
+    Data_str = Serial.read() ; 
+    Data[counter]= Data_str.toInt() ; 
+     
+  }
+  
 }
